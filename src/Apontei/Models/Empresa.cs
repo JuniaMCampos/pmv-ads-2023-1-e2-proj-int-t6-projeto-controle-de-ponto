1bde3f0edf642f8b082c48bb528b0f4a9f350eef
Junia Campos <juniacamposjc@gmail.com>
2023-05-03 20:34:38 -0300

    classe empresa

---
+++ src/Apontei/Models/Empresa.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Apontei.Models
+{
+    public class Empresa
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório Informar o CNPJ")]
+        public string CNPJ { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar Nome")]
+        public string Nome { get; set; }
+
+    }
+}
