3ed205e5f03ed35d87cc078605afe47333799c84
Junia Campos <juniacamposjc@gmail.com>
2023-05-03 20:53:44 -0300

    migration e update-database Gestor

---
+++ src/Apontei/Migrations/20230503235218_Gestor.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Apontei.Migrations
+{
+    public partial class Gestor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "Funcionarios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "GestorId",
+                table: "Funcionarios",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Funcionarios_GestorId",
+                table: "Funcionarios",
+                column: "GestorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Funcionarios_Funcionarios_GestorId",
+                table: "Funcionarios",
+                column: "GestorId",
+                principalTable: "Funcionarios",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Funcionarios_Funcionarios_GestorId",
+                table: "Funcionarios");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Funcionarios_GestorId",
+                table: "Funcionarios");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "Funcionarios");
+
+            migrationBuilder.DropColumn(
+                name: "GestorId",
+                table: "Funcionarios");
+        }
+    }
+}
