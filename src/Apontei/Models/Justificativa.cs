1be75cc748521abc86feea53451023aeffe9fc59
Junia Campos <juniacamposjc@gmail.com>
2023-05-03 21:03:06 -0300

    criação classe justificativa

---
+++ src/Apontei/Models/Justificativa.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+using System;
+
+namespace Apontei.Models
+{
+    public class Justificativa
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "A data é obrigatória.")]
+        public DateTime Data { get; set; }
+
+        [Required(ErrorMessage = "O motivo é obrigatório.")]
+        public string Motivo { get; set; }
+
+        [Display(Name = "Documento")]
+        public string AnexarDocumento { get; set; }
+
+        [Required(ErrorMessage = "o status é obrigatória")]
+        [Display(Name = "Status do Pedido")]
+        public Status Status { get; set; }
+    }
+    public enum Status
+    {
+        Aprovado,
+        Pendente,
+        Reprovado
+    }
+}
