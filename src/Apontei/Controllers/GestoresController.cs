bdb08fa55a4385885bb70a33d998e170d4ba4ad8
Junia Campos <juniacamposjc@gmail.com>
2023-05-04 20:00:59 -0300

    Controllers

---
+++ src/Apontei/Controllers/GestoresController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Apontei.Models;
+
+namespace Apontei.Controllers
+{
+    public class GestoresController : Controller
+    {
+        private readonly AplicationDbContext _context;
+
+        public GestoresController(AplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Gestores
+        public async Task<IActionResult> Index()
+        {
+            var aplicationDbContext = _context.Gestores.Include(g => g.Empresa).Include(g => g.Gestor);
+            return View(await aplicationDbContext.ToListAsync());
+        }
+
+        // GET: Gestores/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gestor = await _context.Gestores
+                .Include(g => g.Empresa)
+                .Include(g => g.Gestor)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (gestor == null)
+            {
+                return NotFound();
+            }
+
+            return View(gestor);
+        }
+
+        // GET: Gestores/Create
+        public IActionResult Create()
+        {
+            ViewData["EmpresaId"] = new SelectList(_context.Empresas, "Id", "CNPJ");
+            ViewData["GestorId"] = new SelectList(_context.Gestores, "Id", "Cargo");
+            return View();
+        }
+
+        // POST: Gestores/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,Sobrenome,Cpf,Pis,Departamento,Cargo,Telefone,Email,Senha,Perfil,ImagemPerfil,EmpresaId,GestorId")] Gestor gestor)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(gestor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmpresaId"] = new SelectList(_context.Empresas, "Id", "CNPJ", gestor.EmpresaId);
+            ViewData["GestorId"] = new SelectList(_context.Gestores, "Id", "Cargo", gestor.GestorId);
+            return View(gestor);
+        }
+
+        // GET: Gestores/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gestor = await _context.Gestores.FindAsync(id);
+            if (gestor == null)
+            {
+                return NotFound();
+            }
+            ViewData["EmpresaId"] = new SelectList(_context.Empresas, "Id", "CNPJ", gestor.EmpresaId);
+            ViewData["GestorId"] = new SelectList(_context.Gestores, "Id", "Cargo", gestor.GestorId);
+            return View(gestor);
+        }
+
+        // POST: Gestores/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Sobrenome,Cpf,Pis,Departamento,Cargo,Telefone,Email,Senha,Perfil,ImagemPerfil,EmpresaId,GestorId")] Gestor gestor)
+        {
+            if (id != gestor.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(gestor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GestorExists(gestor.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmpresaId"] = new SelectList(_context.Empresas, "Id", "CNPJ", gestor.EmpresaId);
+            ViewData["GestorId"] = new SelectList(_context.Gestores, "Id", "Cargo", gestor.GestorId);
+            return View(gestor);
+        }
+
+        // GET: Gestores/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gestor = await _context.Gestores
+                .Include(g => g.Empresa)
+                .Include(g => g.Gestor)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (gestor == null)
+            {
+                return NotFound();
+            }
+
+            return View(gestor);
+        }
+
+        // POST: Gestores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var gestor = await _context.Gestores.FindAsync(id);
+            _context.Gestores.Remove(gestor);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool GestorExists(int id)
+        {
+            return _context.Gestores.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ src/Apontei/Controllers/JustificativasController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Apontei.Models;
+
+namespace Apontei.Controllers
+{
+    public class JustificativasController : Controller
+    {
+        private readonly AplicationDbContext _context;
+
+        public JustificativasController(AplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Justificativas
+        public async Task<IActionResult> Index()
+        {
+            var aplicationDbContext = _context.Justificativas.Include(j => j.Empresa).Include(j => j.Gestor);
+            return View(await aplicationDbContext.ToListAsync());
+        }
+
+        // GET: Justificativas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var justificativa = await _context.Justificativas
+                .Include(j => j.Empresa)
+                .Include(j => j.Gestor)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (justificativa == null)
+            {
+                return NotFound();
+            }
+
+            return View(justificativa);
+        }
+
+        // GET: Justificativas/Create
+        public IActionResult Create()
+        {
+            ViewData["EmpresaId"] = new SelectList(_context.Empresas, "Id", "CNPJ");
+            ViewData["GestorId"] = new SelectList(_context.Gestores, "Id", "Cargo");
+            return View();
+        }
+
+        // POST: Justificativas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Data,Motivo,AnexarDocumento,Status,EmpresaId,GestorId")] Justificativa justificativa)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(justificativa);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmpresaId"] = new SelectList(_context.Empresas, "Id", "CNPJ", justificativa.EmpresaId);
+            ViewData["GestorId"] = new SelectList(_context.Gestores, "Id", "Cargo", justificativa.GestorId);
+            return View(justificativa);
+        }
+
+        // GET: Justificativas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var justificativa = await _context.Justificativas.FindAsync(id);
+            if (justificativa == null)
+            {
+                return NotFound();
+            }
+            ViewData["EmpresaId"] = new SelectList(_context.Empresas, "Id", "CNPJ", justificativa.EmpresaId);
+            ViewData["GestorId"] = new SelectList(_context.Gestores, "Id", "Cargo", justificativa.GestorId);
+            return View(justificativa);
+        }
+
+        // POST: Justificativas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Data,Motivo,AnexarDocumento,Status,EmpresaId,GestorId")] Justificativa justificativa)
+        {
+            if (id != justificativa.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(justificativa);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!JustificativaExists(justificativa.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmpresaId"] = new SelectList(_context.Empresas, "Id", "CNPJ", justificativa.EmpresaId);
+            ViewData["GestorId"] = new SelectList(_context.Gestores, "Id", "Cargo", justificativa.GestorId);
+            return View(justificativa);
+        }
+
+        // GET: Justificativas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var justificativa = await _context.Justificativas
+                .Include(j => j.Empresa)
+                .Include(j => j.Gestor)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (justificativa == null)
+            {
+                return NotFound();
+            }
+
+            return View(justificativa);
+        }
+
+        // POST: Justificativas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var justificativa = await _context.Justificativas.FindAsync(id);
+            _context.Justificativas.Remove(justificativa);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool JustificativaExists(int id)
+        {
+            return _context.Justificativas.Any(e => e.Id == id);
+        }
+    }
+}
